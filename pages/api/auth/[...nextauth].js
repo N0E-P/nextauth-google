8bac5e1636a74b57b3095214e81c5187d0f808c6
N0E-P <pecci.noe@gmail.com>
2023-02-03 14:30:46 +0100

    WORKING demo here

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,28 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl:
-        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
-      scope:
-        'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
-    }),
-  ],
-  jwt: {
-    encryption: true,
-  },
-  secret: process.env.SECRET,
-  callbacks: {
-    async jwt(token, user, account, profile, isNewUser) {
-      // console.log({ profile });
-      if (account?.accessToken) {
-        token.accessToken = account.accessToken;
-      }
-      return token;
-    },
-  },
+	providers: [
+		Providers.Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorizationUrl:
+				"https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+			scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly",
+		}),
+	],
+	jwt: {
+		encryption: true,
+	},
+	secret: process.env.SECRET,
+	callbacks: {
+		async jwt(token, user, account, profile, isNewUser) {
+			// console.log({ profile });
+			if (account?.accessToken) {
+				token.accessToken = account.accessToken;
+				//console.log("token.accessToken", token.accessToken);
+			}
+			return token;
+		},
+	},
 });
--- pages/api/getYTData.js
+++ pages/api/getYTData.js
@@ -1,41 +1,43 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import axios from 'axios';
-import { getSession } from 'next-auth/client';
-import { getToken } from 'next-auth/jwt';
+import axios from "axios";
+import { getSession } from "next-auth/client";
+import { getToken } from "next-auth/jwt";
 
 const secret = process.env.SECRET;
 let accessToken;
 
-const getYTData = async (pageToken = '') => {
-  const { data } = await axios.get(
-    `https://youtube.googleapis.com/youtube/v3/subscriptions?mine=true&pageToken=${pageToken}&maxResults=50&part=snippet`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
+const getYTData = async (pageToken = "") => {
+	const { data } = await axios.get(
+		`https://youtube.googleapis.com/youtube/v3/subscriptions?mine=true&pageToken=${pageToken}&maxResults=50&part=snippet`,
+		{
+			headers: {
+				Authorization: `Bearer ${accessToken}`, //process.env.ACCESSTOKEN
+			},
+		}
+	);
 
-  if (data?.nextPageToken) {
-    return data.items.concat(await getYTData(data.nextPageToken));
-  }
+	if (data?.nextPageToken) {
+		return data.items.concat(await getYTData(data.nextPageToken));
+	}
 
-  return data.items;
+	return data.items;
 };
 
 export default async (req, res) => {
+	/*
   const session = await getSession({ req });
 
   if (!session) {
     return res.status(401).end();
   }
+  */
 
-  const token = await getToken({ req, secret, encryption: true });
+	const token = await getToken({ req, secret, encryption: true });
 
-  accessToken = token.accessToken;
+	accessToken = token.accessToken;
 
-  const data = await getYTData();
+	const data = await getYTData();
 
-  res.status(200).json(data);
+	res.status(200).json(data);
 };
